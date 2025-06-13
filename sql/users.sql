43cfc194083cd57384b747354ed13efd6377e27c
Solène vandercruyssen <solenevandercruyssen2@gmail.com>
2025-06-13 11:50:38 +0200

    docs: pages plateforme, ajout dossier json, config, fichiers blog_config, modif compose.yaml, modif users.sql, ajout fichier session.php

---
+++ sql/users.sql
@@ -0,0 +1,22 @@
+-- base de donnée "QUANTICODE"
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+CREATE DATABASE IF NOT EXISTS `quanticode` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `quanticode`;
+
+-- ---------------------------------------------
+-- Structure de la table users 
+
+
+CREATE TABLE IF NOT EXIST "users" (
+`idUser` int(11) NOT NULL AUTO_INCREMENT,
+`username` varchar(25) NOT NULL,
+`email` varchar(255) NOT NULL,
+`password` text NOT NULL,
+`createdAt` datetime NOT NULL DEFAULT current_timestamp(),
+`editedAt` datetime DEFAULT NULL,
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
