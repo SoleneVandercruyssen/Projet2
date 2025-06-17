0f756ca733dde3524918d1a9f06a9d93314e1f59
solene.vandercruyssen <solenevandercruyssen2@gmail.com>
2025-06-17 15:08:21 +0200

    docs: calendar date

---
+++ JS/date.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const isWeekend = day => {
+  // 
+  return true;
+}
--- JS/plateforme.js
+++ JS/plateforme.js
@@ -1,2 +1,16 @@
 "use strict";
 
+
+const calendar = document.querySelector("#app-calendar");
+
+const isWeekend = day => {
+  return true;
+}
+
+for (let day = 1; day <= 31; day++){
+
+  // ! bien mettre "+=" SINON À chaque itération, on remplaces tout le contenu précédent par le nouveau jour. Et À la fin de la boucle, seul le dernier jour (30) reste affiché.
+
+  const weekend = isWeekend(day);
+    calendar.innerHTML += `<div class ="day weekend"> ${day} </div>`;
+}
