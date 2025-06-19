57f5a5676fb3efef6f16f6e87ddb0fc164f900f4
Solène vandercruyssen <solenevandercruyssen2@gmail.com>
2025-06-19 14:41:57 +0200

    docs: modif sql

--- sql/users.sql
+++ sql/users.sql
@@ -8,11 +8,14 @@ SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS `quanticode` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `quanticode`;
 
-CREATE TABLE IF NOT EXIST "users" (
-`idUser` int(11) NOT NULL AUTO_INCREMENT,
-`username` varchar(25) NOT NULL,
-`email` varchar(255) NOT NULL,
-`password` text NOT NULL,
-`createdAt` datetime NOT NULL DEFAULT current_timestamp(),
-`editedAt` datetime DEFAULT NULL,
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE IF NOT EXISTS `users` (
+    `idUser` int(11) NOT NULL AUTO_INCREMENT,
+    `username` varchar(25) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `password` text NOT NULL,
+    `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `editedAt` datetime DEFAULT NULL,
+    PRIMARY KEY (`idUser`),
+    UNIQUE(id), 
+    UNIQUE(email)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
